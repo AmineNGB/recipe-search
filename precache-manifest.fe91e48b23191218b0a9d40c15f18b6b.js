4a752181babb14993722817474159839660bbec1
amine neghbel <amineneghbel@gmail.com>
2020-08-09 20:02:32 +0200

    Updates

---
+++ precache-manifest.fe91e48b23191218b0a9d40c15f18b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56818ae17ef7adc0435909a26afaf0de",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "3a641e89817fd7b75898",
+    "url": "/recipe-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b948b7800e21a39401d",
+    "url": "/recipe-search/static/js/2.db5cd27f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-search/static/js/2.db5cd27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a641e89817fd7b75898",
+    "url": "/recipe-search/static/js/main.2facfeab.chunk.js"
+  },
+  {
+    "revision": "a6266fd59a6f68d64d74",
+    "url": "/recipe-search/static/js/runtime-main.de634d92.js"
+  }
+]);
